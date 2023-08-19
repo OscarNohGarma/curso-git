0c85a33d83a46de95e633ed6b0e6ceee5413ef0d
Oscar Garma <spongetcrazy@gmail.com>
2023-08-18 22:04:11 -0600

    Connect repository to laptop

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const url = "https://jsonplaceholder.typicode.com";
 const getUser = async (id) => {
   const res = await fetch(`${url}/users?id=${id}`);
   const user = (await res.json())[0];
-
   return user;
 }
 
@@ -15,20 +14,20 @@ const getPosts = async (user) => {
 }
 
 const getCommentsForEachPost = async (posts) => {
-  const res = await Promise.all(posts.map(post => 
+  const res = await Promise.all(posts.map(post =>
     fetch(`${url}/comments?postId=${post.id}&_limit=4`)
-    
+
   ));
   console.log(res);
   const postComments = await Promise.all(res.map(r => r.json()));
-  
+
   postComments.forEach((comments, i) => posts[i].comments = comments);
 }
 
 const renderHtml = (user, posts) => {
   const content = document.getElementById('content');
   content.innerHTML += `<h3>Posts del usuario ${user.email}</h3>`;
-  
+
   posts.forEach(post => {
     content.innerHTML += `
     <div class="post">
